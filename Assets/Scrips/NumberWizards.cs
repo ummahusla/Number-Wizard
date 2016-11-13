64bd38b75b92723d824a32b029fe6f72b024e9e3
Edvins Antonovs <ummahusla@gmail.com>
2016-11-13 20:36:32 +0000

    Minor refactoring, added helper functions

--- Assets/Scrips/NumberWizards.cs
+++ Assets/Scrips/NumberWizards.cs
@@ -9,15 +9,25 @@ public class NumberWizards : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		StartGame();
+	}
+	
+	// Printing out the game description
+	void StartGame() {
 	
 		max = max + 1;
-	
+		
 		print("Welcome to Number Wizard!");
 		print("Pick a number in your head, but don't tell to me.");
 		print("The lowest number you can pick is " + min + " while the highest number is " + max + ".");
 		print("Is the number is higher or lower than "+ guess + "?");
 		print("Up arrow = higher, down = lower, return = equal.");
+		
+	}
 	
+	// Make the next guess
+	void NextGuess() {
+		guess = (max + min) / 2;
 	}
 	
 	// Update is called once per frame
@@ -26,13 +36,13 @@ public class NumberWizards : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.UpArrow)) {
 		
 			min = guess;
-			guess = (max + min) / 2;
+			NextGuess();
 			print("Higher or lower than " + guess);
 			
 		} else if(Input.GetKeyDown(KeyCode.DownArrow)) {
 		
 			max = guess;
-			guess = (max + min) / 2;
+			NextGuess();
 			print("Higher or lower than " + guess);
 			
 		} else if(Input.GetKeyDown(KeyCode.Return)) {
