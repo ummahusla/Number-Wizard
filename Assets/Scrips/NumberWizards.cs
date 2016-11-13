3d07820e45f331fc5469db2b8e87d8ee1390d57d
Edvins Antonovs <ummahusla@gmail.com>
2016-11-13 18:59:28 +0000

    Added print statement on run

--- Assets/Scrips/NumberWizards.cs
+++ Assets/Scrips/NumberWizards.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NumberWizard : MonoBehaviour {
+public class NumberWizards : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		print("Welcome to Number Wizard!");
 	}
 	
 	// Update is called once per frame
