4e2fcd8ee913c4cb553035e2349db1f3ae47e43a
Edvins Antonovs <ummahusla@gmail.com>
2016-11-13 20:29:49 +0000

    Added a basic game functionality

--- Assets/Scrips/NumberWizards.cs
+++ Assets/Scrips/NumberWizards.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class NumberWizards : MonoBehaviour {
 
+	int min = 1;
+	int max = 1000;
+	int guess = 500;
+	
 	// Use this for initialization
 	void Start () {
 	
-		int max = 1000;
-		int min = 1;
-		
+		max = max + 1;
+	
 		print("Welcome to Number Wizard!");
 		print("Pick a number in your head, but don't tell to me.");
 		print("The lowest number you can pick is " + min + " while the highest number is " + max + ".");
-		print("Is the number is higher or lower than 500?");
+		print("Is the number is higher or lower than "+ guess + "?");
 		print("Up arrow = higher, down = lower, return = equal.");
 	
 	}
@@ -21,11 +24,21 @@ public class NumberWizards : MonoBehaviour {
 	void Update () {
 	
 		if(Input.GetKeyDown(KeyCode.UpArrow)) {
-			print("Up arrow is pressed.");
+		
+			min = guess;
+			guess = (max + min) / 2;
+			print("Higher or lower than " + guess);
+			
 		} else if(Input.GetKeyDown(KeyCode.DownArrow)) {
-			print("Down arrow is pressed.");	
+		
+			max = guess;
+			guess = (max + min) / 2;
+			print("Higher or lower than " + guess);
+			
 		} else if(Input.GetKeyDown(KeyCode.Return)) {
-			print("Return key is pressed.");
+		
+			print("Victory!");
+			
 		}
 		
 	}
