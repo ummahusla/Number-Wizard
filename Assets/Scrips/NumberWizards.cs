9e7c4b32bc2020c432aa4d2ed04714ec273de1e9
Edvins Antonovs <ummahusla@gmail.com>
2016-11-13 19:12:52 +0000

    Added min and max numbers

--- Assets/Scrips/NumberWizards.cs
+++ Assets/Scrips/NumberWizards.cs
@@ -6,6 +6,12 @@ public class NumberWizards : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print("Welcome to Number Wizard!");
+		print("Pick a number in your head, but don't tell to me.");
+		
+		int max = 1000;
+		int min = 1;
+		
+		print("The highest number you can pick is " + max + ".");
 	}
 	
 	// Update is called once per frame
